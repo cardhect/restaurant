f6c6d65cb196a1adf9c22dd785e7ad7867beaf1c
cardhect <card.hectorl@gmail.com>
2022-07-20 11:23:42 -0500

    added links to nav, added image to main-body, added font for site-name

--- src/nav.js
+++ src/nav.js
@@ -1,10 +1,33 @@
-const nav = function() {
-    const nav = document.createElement('div');
-    nav.setAttribute('id','nav-bar');
+const nav = function () {
+	let linksArray = ["Home", "Menu", "About", "Contact"];
+	//created elements
+    const buttonContainer = document.createElement('div');
+        buttonContainer.setAttribute('id','button-container');  
+	const nav = document.createElement("div");
+	    nav.setAttribute("id", "nav-bar");
+	const siteName = document.createElement("h1");
+        siteName.setAttribute("id", "site-name");
+        siteName.textContent = "Cocina-Cardenas";
+	
+     //dom elements
+	const contentDiv = document.getElementById("content");
+	    contentDiv.append(nav); 
 
-    const contentDiv = document.getElementById('content');
+        const navBar = document.getElementById("nav-bar");
+        navBar.append(siteName);
+        navBar.append(buttonContainer)
 
-    contentDiv.append(nav);
-}
 
-export { nav }
+	for (let index = 0; index < linksArray.length; index++) {
+        const element = linksArray[index];
+		
+        const links = document.createElement("button");
+		    links.setAttribute("class", "nav-links");
+            links.textContent = element;
+		
+        buttonContainer.append(links)
+		console.log(element);
+	}
+};
+
+export { nav };
